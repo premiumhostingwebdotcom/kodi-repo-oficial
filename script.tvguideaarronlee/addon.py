f8cc008cf320a2bed9caf37476e6f857550e3c0c
premiumhostingwebdotcom <tvguideaarronlee@gmail.com>
2015-07-22 20:17:50 +0200

    Kodi 15.0 Compatible

--- script.tvguideaarronlee/addon.py
+++ script.tvguideaarronlee/addon.py
@@ -30,7 +30,7 @@ xbmc.Player().stop
 ADDON       = xbmcaddon.Addon(id = 'script.tvguideaarronlee')
 HOME        = ADDON.getAddonInfo('path')
 TITLE       = 'DigiTele TV Esp'
-VERSION     = '5.4.3.1'
+VERSION     = '5.4.3.2'
 addon       = xbmcaddon.Addon()
 addonid     = addon.getAddonInfo('id')
 versioninfo = addon.getAddonInfo('version')
